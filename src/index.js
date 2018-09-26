af048d6058128bf4a160de0944991cc1c0240330
OLGA NARANOVICH <naranovicholga@gmail.com>
2018-09-26 22:44:25 +0300

    additional_5

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   const arr = str.split('');
 	while (arr.length > 0) {
-	  for (let i = 0, len = arr.length; i < len; i++) {
-	  	for (let j = 0, len = bracketsConfig.length; j < len; j++) {
-  		
-  			if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) {
-  				arr.splice(i, 2);
-  				return true;
-  			} else { 
-  				return false;
-  			}
-  			
-  		}
-  	} 
-  } 
+		for (let i = 0, len = arr.length; i < len; i++) {
+			for (let j = 0, len = bracketsConfig.length; j < len; j++) {
+			
+				if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) {
+					arr.splice(i, 2); 
+					if (arr.length === 0) {
+						return true;
+					}
+				}	
+			} continue;
+		}
+		if (arr.length > 0) { 
+			break;
+		} 
+	} return false;
 }
