567cfa2372eae1654b7fcbdc28e1f1b6ceaaef7a
OLGA NARANOVICH <naranovicholga@gmail.com>
2018-09-26 16:45:36 +0300

    additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = str.split('');
+	while (arr.length > 0) {
+	  for (let i = 0, len = arr.length; i < len; i++) {
+	  	for (let j = 0, len = bracketsConfig.length; j < len; j++) {
+  		
+  			if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) {
+  				arr.splice(i, 2);
+  				return true;
+  			} else { 
+  				return false;
+  			}
+  			
+  		}
+  	} 
+  } 
 }
